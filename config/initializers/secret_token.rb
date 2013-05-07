c88386c595588d626b92b224c0aa99ea96219ba2
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-07 11:16:59 +0900

    install rails4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Example::Application.config.secret_key_base = '0356963138b25e7103579841ed4ca9fd2670355e0c589dea9365a9b01af711f44e5fa2ce2dd3c1ac0a2babd3806c956373959487137a9cb8d45fd09d27ee58d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Example::Application.config.session_store :encrypted_cookie_store, key: '_example_session'
