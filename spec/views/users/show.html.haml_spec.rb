7d7349b50f7c6ca208b8bff22264984d8a436d33
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-08 18:42:45 +0900

    users#show のボタン制御

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -1,13 +1,24 @@
 require 'spec_helper'
 
 describe "users/show" do
-  before(:each) do
-    @user = assign(:user, FactoryGirl.create(:user))
+  subject { render; response.body }
+  let(:current_user){ FactoryGirl.create(:user) }
+  before do
+    controller.stub(current_user: current_user)
+    view.stub(current_user: current_user)
+    assign(:current_ability, Ability.new(current_user))
+
+    assign(:user, user)
   end
 
-  it "renders attributes in <p>" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    expect(rendered).to match(@user.username)
+  context "login user" do
+    let(:user){ current_user }
+    it { expect(view.can?(:update, user)).to be_true }
+    it { should have_selector :a, href: edit_user_path(user) }
+  end
+
+  context "another user" do
+    let(:user){ FactoryGirl.create(:user) }
+    it { should_not have_xpath "//a[contains(@href,'/edit')]" }
   end
 end
