d194441d6ab2695e0a5e5925fdad1f8084e474c1
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-09 19:24:25 +0900

    User と Post に dependent: :destroy の追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :user, presence: true
   validates :text, presence: true, length: 1..300
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
 end
