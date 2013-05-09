a0f01769b956219af29eb4fa6ecac5ad9d709c6e
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-09 19:22:57 +0900

    validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post, counter_cache: true
 
+  validates :user, presence: true
+  validates :post, presence: true
   validates :body, presence: true, length: 1..140
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
+
   validates :username, presence: true, uniqueness: true
 end
