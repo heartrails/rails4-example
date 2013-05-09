bf57857231151c4cb3c3e8ed0070a011e7b1cb0a
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-09 19:52:33 +0900

    gem 'activerecord-session_store'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130507112222) do
+ActiveRecord::Schema.define(version: 20130509105127) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id",    null: false
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 20130507112222) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
   create_table "users", force: true do |t|
     t.string   "username",        null: false
