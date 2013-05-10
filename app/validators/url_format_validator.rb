e0748526c2d73cd1e50126d60db4dc8d82433fff
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-10 11:22:40 +0900

    UrlValidator の実装ミス

--- app/validators/url_format_validator.rb
+++ app/validators/url_format_validator.rb
@@ -1,6 +1,6 @@
 class UrlFormatValidator < ActiveModel::EachValidator
   def self.valid?(value)
-    URI.parse(value).kind_of?(URI::HTTP)
+    URI.parse(value).kind_of?(URI::HTTP) rescue false
   end
   def validate_each(object, attribute, value)
     object.errors[attribute] << (options[:message] || "の形式が不正です") unless UrlFormatValidator.valid?(value)
