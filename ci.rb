de15004835343d67e4e21dd419b562fbee77d774
ssig33 <mail@ssig33.com>
2013-05-10 12:15:08 +0900

    Heroku への自動デプロイ

--- ci.rb
+++ ci.rb
@@ -1,6 +1,5 @@
 require 'fileutils'
-system "bundle --path=vendor/bundle"
+system "bundle --path=vendor/bundle" unless ARGV[0] == 'travis'
 FileUtils.rm "db/test.sqlite3" if File.exist? "db/test.sqlite3"
 FileUtils.cp "config/database.yml.example", "config/database.yml"
 exit system "bundle exec rake db:migrate ci:setup:rspec spec RAILS_ENV=test"
-FileUtils.rm "config/database.yml"
