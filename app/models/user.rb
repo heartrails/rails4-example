11d74d42d3b74c35fcaab972b8c7e3dd15508b32
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-10 11:23:13 +0900

    username の validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, uniqueness: true, length: 3..12, format: /\A[a-z0-9_.]+\z/
 end
