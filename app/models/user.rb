a507d69929855997297042a5d7c913be3b6b7905
Ohkubo KOHEI <ohkubo@magician.jp>
2013-05-13 11:56:19 +0900

    User の validtion 強化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :username, uniqueness: true, length: 3..12, format: /\A[a-z0-9_.]+\z/
+  validates :username, presence: true, uniqueness: true, length: 3..12, format: /\A[a-z0-9_.]+\z/
+  validates :password, length: { minimum: 6 }, unless: ->{ password_digest.present? }
 end
